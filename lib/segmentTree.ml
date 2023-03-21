217009e09e11f59d88d4a933eec34b5f8569ce3d
samosica <samosic4@gmail.com>
2023-03-21 23:38:54 +0900

    Remove unnecessary code

--- lib/segmentTree.ml
+++ lib/segmentTree.ml
@@ -4,19 +4,6 @@ module type MonoidType = sig
   val mul : t -> t -> t
 end
 
-module Bit = struct
-  let leftmost_bit n =
-    let () = assert (n > 0) in
-    let rec loop n w r =
-      if w = 0 then
-        r
-      else if n lsr w > 0 then
-        loop (n lsr w) (w / 2) (r + w)
-      else
-        loop n (w / 2) r in
-    loop n 32 0
-end
-
 module SegmentTree : sig
   module type S = sig
     type elt
