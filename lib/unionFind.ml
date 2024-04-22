bbb9f0b0c01525182f9048aa784529032b4f2dd7
samosica <samosic4@gmail.com>
2024-04-23 02:31:39 +0900

    Add is_root and size function for Union-Find trees

--- lib/unionFind.ml
+++ lib/unionFind.ml
@@ -2,16 +2,20 @@ module UnionFind : sig
   type t
   val make : int -> t
   val root : int -> t -> int
+  val is_root : int -> t -> bool
   val unite : int -> int -> t -> unit
+  val size : int -> t -> int
 end = struct
   type t = {
     parent : int array;
-    rank : int array
+    rank : int array;
+    size : int array;
   }
 
   let make n = {
     parent = Array.init n Fun.id;
-    rank = Array.make n 0
+    rank = Array.make n 0;
+    size = Array.make n 1;
   }
 
   let rec root v t =
@@ -22,17 +26,23 @@ end = struct
       t.parent.(v) <- r;
       r
 
+  let is_root v t = v = root v t
+
   let unite v w t =
     let v = root v t in
     let w = root w t in
     if v = w then
       ()
-    else if t.rank.(v) > t.rank.(w) then
-      t.parent.(w) <- v
-    else begin
+    else if t.rank.(v) > t.rank.(w) then begin
+      t.parent.(w) <- v;
+      t.size.(v) <- t.size.(v) + t.size.(w)
+    end else begin
       t.parent.(v) <- w;
+      t.size.(w) <- t.size.(w) + t.size.(v);
       if t.rank.(v) = t.rank.(w) then begin
         t.rank.(w) <- t.rank.(w) + 1
       end
     end
+
+  let size v t = t.size.(root v t)
 end
