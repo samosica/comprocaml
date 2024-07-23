19040e27698de63b96c57d7f2e84cfb4b7985572
samosica <samosic4@gmail.com>
2024-07-23 17:30:49 +0900

    Add replace function

--- lib/arrayExt.ml
+++ lib/arrayExt.ml
@@ -1,3 +1,6 @@
+(** Apply a function to an element of an array *)
+let[@inline] replace a i f = a.(i) <- f a.(i)
+
 (** Compute the next permutation of an array [a] in the lexicographic order.
     Just return [false] if [a] is the last permutation;
     otherwise modify [a] and return [true].
