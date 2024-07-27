aed042e87eb2ca4c347f34cf23a4e6974ebf2238
samosica <samosic4@gmail.com>
2024-07-27 15:46:00 +0900

    Add tests

--- test/print.ml
+++ test/print.ml
@@ -1,5 +1,44 @@
 open Comprocaml
 
+let%expect_test "output_list(empty)" =
+  Printf.printf "%a\n" (Print.output_list output_char) [];
+  [%expect{|
+  |}]
+
+let%expect_test "output_list(nonempty) with sep" =
+  Printf.printf "%a\n" (Print.output_list ~sep:", " output_char) ['a'; 'b'; 'c'];
+  [%expect{|
+    a, b, c
+  |}]
+
+let%expect_test "output_int_list" =
+  Printf.printf "%a\n" (Print.output_int_list()) [1; 33; 100];
+  [%expect{|
+    1 33 100
+  |}]
+
+let%expect_test "output_string_list" =
+  Printf.printf "%a\n" (Print.output_string_list()) ["子"; "丑"; "寅"];
+  [%expect{|
+    子 丑 寅
+  |}]
+
+let%expect_test "print_int_list" =
+  Print.print_int_list [1; 33; 100];
+  print_string "!"; (* check a newline is inserted *)
+  [%expect{|
+    1 33 100
+    !
+  |}]
+
+let%expect_test "output_string_list" =
+  Print.print_string_list ["子"; "丑"; "寅"];
+  print_string "!"; (* check a newline is inserted *)
+  [%expect{|
+    子 丑 寅
+    !
+  |}]
+
 let%expect_test "output_bits(0)" =
   Printf.printf "%a\n" (Print.output_bits ()) 0;
   [%expect{|
