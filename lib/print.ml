8f3747eea3b39759593d8abe00601280fcaddc1f
samosica <samosic4@gmail.com>
2024-07-27 13:37:00 +0900

    Generalize print_int_list to arbitrary types

--- lib/print.ml
+++ lib/print.ml
@@ -1,7 +1,26 @@
-let rec print_int_list = function
-| [] -> ()
-| [x] -> Printf.printf "%d\n" x
-| x :: l -> Printf.printf "%d " x; print_int_list l
+let output_list ?(sep = " ") output ch l =
+  match l with
+  | x :: l' ->
+    output ch x;
+    List.iter (fun y ->
+      output_string ch sep;
+      output ch y
+    ) l'
+  | _ -> ()
+
+let output_int_list ?(sep = " ") () ch l =
+  output_list ~sep (fun ch -> Printf.fprintf ch "%d") ch l
+
+let output_string_list ?(sep = " ") () ch l =
+  output_list ~sep output_string ch l
+
+let print_int_list ?(sep = " ") ?(end_ = "\n") l =
+  output_int_list ~sep () stdout l;
+  print_string end_
+
+let print_string_list ?(sep = " ") ?(end_ = "\n") l =
+  output_string_list ~sep () stdout l;
+  print_string end_
 
 let output_bits ?width () ch n =
   assert (n >= 0);
--- lib/print.mli
+++ lib/print.mli
@@ -1,10 +1,39 @@
-val print_int_list : int list -> unit
+(** Print a list of an arbitrary type.
+    Elements are separated by [sep] (the default value is a single space).
+
+    Usage:
+    - [Printf.printf "%a" (Print.output_list (fun ch -> Printf.fprintf ch "%d")) [...]]
+    - [Printf.printf "%a" (Print.output_list output_string) [...]]
+  *)
+val output_list : ?sep:string -> (out_channel -> 'a -> unit) -> out_channel -> 'a list -> unit
+
+(** Print a list of integers.
+    Elements are separated by [sep] (the default value is a single space).
+
+    Usage:
+    - [Printf.printf "%a" (Print.output_int_list()) [...]]
+    - [Printf.printf "%a" (Print.output_int_list ~sep:"..." ()) [...]]
+  *)
+val output_int_list : ?sep:string -> unit -> out_channel -> int list -> unit
+
+(** Print a list of strings.
+    Elements are separated by [sep] (the default value is a single space).
+
+    Usage:
+    - [Printf.printf "%a" (Print.output_string_list()) [...]]
+    - [Printf.printf "%a" (Print.output_string_list ~sep:"..." ()) [...]]
+  *)
+val output_string_list : ?sep:string -> unit -> out_channel -> string list -> unit
+
+val print_int_list : ?sep:string -> ?end_:string -> int list -> unit
+
+val print_string_list : ?sep:string -> ?end_:string -> string list -> unit
 
 (** Print an integer in binary representation.
     The length is as short as possible,
     and if [width] is specified, it is at least [width].
 
-      Usage:
+    Usage:
     - [Printf.printf "%a" (Print.output_bits()) 10] -> 1010
     - [Printf.printf "%a" (Print.output_bits ~width:7 ()) 10] -> 0001010
   *)
