16a00771daf63c66308c112a3cbd8d08e2fcd259
samosica <samosic4@gmail.com>
2024-07-27 12:07:07 +0900

    Add output_bits function

--- lib/print.ml
+++ lib/print.ml
@@ -2,3 +2,11 @@ let rec print_int_list = function
 | [] -> ()
 | [x] -> Printf.printf "%d\n" x
 | x :: l -> Printf.printf "%d " x; print_int_list l
+
+let output_bits ?width () ch n =
+  assert (n >= 0);
+  let min_width = if n > 0 then Base.Int.floor_log2 n + 1 else 1 in
+  let width = Int.max (Option.value ~default:0 width) min_width in
+  for i = width - 1 downto 0 do
+    output_char ch (Char.chr (n lsr i land 1 + Char.code '0'))
+  done
--- lib/print.mli
+++ lib/print.mli
@@ -1,1 +1,11 @@
 val print_int_list : int list -> unit
+
+(** Print an integer in binary representation.
+    The length is as short as possible,
+    and if [width] is specified, it is at least [width].
+
+      Usage:
+    - [Printf.printf "%a" (Print.output_bits()) 10] -> 1010
+    - [Printf.printf "%a" (Print.output_bits ~width:7 ()) 10] -> 0001010
+  *)
+val output_bits : ?width:int -> unit -> out_channel -> int -> unit
