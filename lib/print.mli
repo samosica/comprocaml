ed79051f07458b219f3b37abf657985ffe040745
samosica <samosic4@gmail.com>
2024-08-02 17:08:11 +0900

    Add print functions for arrays

--- lib/print.mli
+++ lib/print.mli
@@ -1,3 +1,4 @@
+(* Lists *)
 (** Print a list of an arbitrary type.
     Elements are separated by [sep] (the default value is a single space).
 
@@ -29,6 +30,98 @@ val print_int_list : ?sep:string -> ?end_:string -> int list -> unit
 
 val print_string_list : ?sep:string -> ?end_:string -> string list -> unit
 
+(* Subarrays *)
+(** Print a part of an array of an arbitrary type.
+    A subarray of [a] from the [l]-th element (inclusive) to the [r]-th
+    element (exclusive) is represented by a triple [(a, l, r)].
+
+    Elements are separated by [sep] (the default value is a single space).
+
+    Usage:
+    - [Printf.printf "%a" (Print.output_subarray (fun ch -> Printf.fprintf ch "%d")) [| ... |]]
+    - [Printf.printf "%a" (Print.output_subarray output_string) [| ... |]]
+  *)
+val output_subarray : 
+  ?sep:string -> (out_channel -> 'a -> unit) -> out_channel ->
+    ('a array * int * int) -> unit
+
+(** Print a part of an array of integers.
+    A subarray of [a] from the [l]-th element (inclusive) to the [r]-th
+    element (exclusive) is represented by a triple [(a, l, r)].
+
+    Elements are separated by [sep] (the default value is a single space).
+
+    Usage:
+    - [Printf.printf "%a" (Print.output_int_subarray()) [| ... |]]
+    - [Printf.printf "%a" (Print.output_int_subarray ~sep:"..." ()) [| ... |]]
+  *)
+val output_int_subarray : 
+  ?sep:string -> unit -> out_channel -> (int array * int * int) -> unit
+
+(** Print a part of an array of strings.
+    A subarray of [a] from the [l]-th element (inclusive) to the [r]-th
+    element (exclusive) is represented by a triple [(a, l, r)].
+
+    Elements are separated by [sep] (the default value is a single space).
+
+    Usage:
+    - [Printf.printf "%a" (Print.output_string_subarray()) [| ... |]]
+    - [Printf.printf "%a" (Print.output_string_subarray ~sep:"..." ()) [| ... |]]
+  *)
+val output_string_subarray : 
+  ?sep:string -> unit -> out_channel -> (string array * int * int) -> unit
+
+val print_subarray : 
+  ?sep:string -> ?end_:string -> (out_channel -> 'a -> unit) ->
+    ('a array * int * int) -> unit
+
+val print_int_subarray : 
+  ?sep:string -> ?end_:string -> (int array * int * int) -> unit
+
+val print_string_subarray : 
+  ?sep:string -> ?end_:string -> (string array * int * int) -> unit
+
+(* Arrays *)
+(** Print an array of an arbitrary type.
+    Elements are separated by [sep] (the default value is a single space).
+
+    Usage:
+    - [Printf.printf "%a" (Print.output_array (fun ch -> Printf.fprintf ch "%d")) [| ... |]]
+    - [Printf.printf "%a" (Print.output_array output_string) [| ... |]]
+  *)
+val output_array : 
+  ?sep:string -> (out_channel -> 'a -> unit) -> out_channel -> 'a array -> unit
+
+(** Print an array of integers.
+    Elements are separated by [sep] (the default value is a single space).
+
+    Usage:
+    - [Printf.printf "%a" (Print.output_int_array()) [| ... |]]
+    - [Printf.printf "%a" (Print.output_int_array ~sep:"..." ()) [| ... |]]
+  *)
+val output_int_array : 
+  ?sep:string -> unit -> out_channel -> int array -> unit
+
+(** Print an array of strings.
+    Elements are separated by [sep] (the default value is a single space).
+
+    Usage:
+    - [Printf.printf "%a" (Print.output_string_array()) [| ... |]]
+    - [Printf.printf "%a" (Print.output_string_array ~sep:"..." ()) [| ... |]]
+  *)
+val output_string_array : 
+  ?sep:string -> unit -> out_channel -> string array -> unit
+
+val print_array : 
+  ?sep:string -> ?end_:string -> (out_channel -> 'a -> unit) -> 'a array -> unit
+
+val print_int_array : 
+  ?sep:string -> ?end_:string -> int array -> unit
+
+val print_string_array : 
+  ?sep:string -> ?end_:string -> string array -> unit
+
+(* Bits *)
 (** Print an integer in binary representation.
     The length is as short as possible,
     and if [width] is specified, it is at least [width].
--- test/print.ml
+++ test/print.ml
@@ -1,5 +1,6 @@
 open Comprocaml
 
+(* Lists *)
 let%expect_test "output_list(empty)" =
   Printf.printf "%a\n" (Print.output_list output_char) [];
   [%expect{|
@@ -31,7 +32,7 @@ let%expect_test "print_int_list" =
     !
   |}]
 
-let%expect_test "output_string_list" =
+let%expect_test "print_string_list" =
   Print.print_string_list ["子"; "丑"; "寅"];
   print_string "!"; (* check a newline is inserted *)
   [%expect{|
@@ -39,6 +40,77 @@ let%expect_test "output_string_list" =
     !
   |}]
 
+(* Subarrays *)
+let%expect_test "output_subarray(nonempty) with sep" =
+  Printf.printf "%a\n"
+    (Print.output_subarray ~sep:", " output_char)
+    ([| 'a'; 'b'; 'c'; 'd' |], 1, 3);
+  [%expect{|
+    b, c
+  |}]
+
+let%expect_test "output_subarray(empty)" =
+  Printf.printf "%a\n"
+    (Print.output_subarray output_char)
+    ([| 'a'; 'b'; 'c'; 'd' |], 3, 1);
+  [%expect{|
+  |}]
+
+let%expect_test "output_int_subarray" =
+  Printf.printf "%a\n" (Print.output_int_subarray()) ([| 0; 1; 2; 3; 4 |], 1, 4);
+  [%expect{|
+    1 2 3
+  |}]
+
+let%expect_test "output_string_subarray" =
+  Printf.printf "%a\n"
+    (Print.output_string_subarray())
+    ([| "子"; "丑"; "寅" |], 0, 2);
+  [%expect{|
+    子 丑
+  |}]
+
+let%expect_test "print_int_subarray" =
+  Print.print_int_subarray ([| 1; 33; 108; 100 |], 0, 4);
+  print_string "!"; (* check a newline is inserted *)
+  [%expect{|
+    1 33 108 100
+    !
+  |}]
+
+let%expect_test "print_string_subarray" =
+  Print.print_string_subarray ([| "子"; "丑"; "寅"; "卯"; "辰" |], 3, 5);
+  print_string "!"; (* check a newline is inserted *)
+  [%expect{|
+    卯 辰
+    !
+  |}]
+
+(* Arrays *)
+let%expect_test "output_array(nonempty)" =
+  Printf.printf "%a\n"
+    (Print.output_array output_char)
+    [| '3'; '2'; '1'; '0' |];
+  [%expect{|
+    3 2 1 0
+  |}]
+
+let%expect_test "output_array(empty)" =
+  Printf.printf "%a\n"
+    (Print.output_array output_char)
+    [||];
+  [%expect{|
+  |}]
+
+let%expect_test "print_int_array" =
+  Print.print_int_array [| 1; 1; 2; 3; 5 |];
+  print_string "!"; (* check a newline is inserted *)
+  [%expect{|
+    1 1 2 3 5
+    !
+  |}]
+
+(* Bits *)
 let%expect_test "output_bits(0)" =
   Printf.printf "%a\n" (Print.output_bits ()) 0;
   [%expect{|
