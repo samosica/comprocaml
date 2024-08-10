688849d02ea2715851a5ba5747e834fb2ede1ee9
samosica <samosic4@gmail.com>
2024-08-11 02:03:02 +0900

    Fix test/graph.ml

--- test/graph.ml
+++ test/graph.ml
@@ -1,35 +1,6 @@
 open Comprocaml
 open Graph
 
-let (let>) i f = Iter.flat_map f i
-
-(** Return how many nodes were visited before each node. *)
-let inv ~n ord =
-  let res = Array.make n 0 in
-  ord |> Array.iteri (fun i v ->
-    res.(v) <- i
-  );
-  res
-
-let imply b c = not b || c
-let iff b c = imply b c && imply c b
-
-let valid_order ~n ~dist ord =
-  let inv_ord = inv ~n ord in
-  Iter.for_all Fun.id @@
-    let> i = Iter.(0 -- (n - 1)) in
-    let> j = Iter.(0 -- (n - 1)) in
-    Iter.return (
-      dist.(i) = -1
-        || dist.(j) = -1
-        || dist.(i) = dist.(j)
-        (*
-          node [i] occurs earlier than node [j] in a (valid) BFS order
-          <=> node [i] is closer to starting nodes than node [j]
-        *)
-        || iff (inv_ord.(i) < inv_ord.(j)) (dist.(i) < dist.(j))
-  )
-
 let%test "bfs(tree)" =
   let n = 7 in
   let g = [|
@@ -47,12 +18,10 @@ let%test "bfs(tree)" =
     Iter.singleton 0
     |> bfs ~g ~dist
     |> Iter.to_array in
-  let ok1 = dist = [| 0; 1; 1; 2; 2; 3; 2 |] in
-  let ok2 =
-    Array.length ord = n
-      && Iter.(for_all (fun i -> Array.mem i ord) (0 -- (n - 1))) in
-  let ok3 = valid_order ~n ~dist ord in
-  ok1 && ok2 && ok3
+  dist = [| 0; 1; 1; 2; 2; 3; 2 |]
+    && Array.length ord = n
+    && Iter.(for_all (fun i -> Array.mem i ord) (0 -- (n - 1)))
+    && Base.Array.is_sorted ~compare:(fun v w -> Int.compare dist.(v) dist.(w)) ord
 
 let%test "bfs(rectangle)" =
   let n = 4 in
@@ -65,12 +34,10 @@ let%test "bfs(rectangle)" =
     Iter.singleton 1
     |> bfs ~g ~dist
     |> Iter.to_array in
-  let ok1 = dist = [| 1; 0; 1; 2 |] in
-  let ok2 =
-    Array.length ord = n
-      && Iter.(for_all (fun i -> Array.mem i ord) (0 -- (n - 1))) in
-  let ok3 = valid_order ~n ~dist ord in
-  ok1 && ok2 && ok3
+  dist = [| 1; 0; 1; 2 |]
+    && Array.length ord = n
+    && Iter.(for_all (fun i -> Array.mem i ord) (0 -- (n - 1)))
+    && Base.Array.is_sorted ~compare:(fun v w -> Int.compare dist.(v) dist.(w)) ord
 
 let%test "dijkstra(tree)" =
   let n = 7 in
@@ -92,10 +59,8 @@ let%test "dijkstra(tree)" =
     Iter.singleton 0
     |> dijkstra ~g ~dist
     |> Iter.to_array in
-  let ok1 = dist = [| 0; 2; 1; 5; 3; 6; 4 |] in
-  (* no ambiguity *)
-  let ok2 = ord = [| 0; 2; 1; 4; 6; 3; 5 |] in
-  ok1 && ok2
+  dist = [| 0; 2; 1; 5; 3; 6; 4 |]
+    && ord = [| 0; 2; 1; 4; 6; 3; 5 |] (* no ambiguity *)
 
 let%test "dijkstra(haste makes waste)" =
   let n = 200 in
@@ -110,13 +75,10 @@ let%test "dijkstra(haste makes waste)" =
     Iter.singleton 0
     |> dijkstra ~g ~dist
     |> Iter.to_array in
-  let ok1 =
-    dist.(0) = 0
-      && dist.(n - 1) = 299
-      && Iter.(for_all (fun v -> dist.(v) = v) (1 -- (n - 2))) in
-  (* no ambiguity *)
-  let ok2 = ord = Array.init n Fun.id in
-  ok1 && ok2
+  dist.(0) = 0
+    && dist.(n - 1) = 299
+    && Iter.(for_all (fun v -> dist.(v) = v) (1 -- (n - 2)))
+    && ord = Array.init n Fun.id (* no ambiguity *)
 
 let%test "compl_bfs rejects not sorted adjacency list" =
   let g = [|
@@ -145,9 +107,9 @@ let%test "compl_bfs(path)" =
   let ord =
     Iter.singleton 0
     |> compl_bfs ~g ~dist
-    |> Iter.to_list in
+    |> Iter.to_array in
   dist = [| 0; 3; 1; 2 |]
-    && ord = [0; 2; 3; 1] (* no ambiguity *)
+    && ord = [| 0; 2; 3; 1 |] (* no ambiguity *)
 
 let%test "compl_bfs(directed)" =
   let n = 4 in
@@ -165,10 +127,10 @@ let%test "compl_bfs(directed)" =
   let ord =
     Iter.singleton 0
     |> compl_bfs ~g ~dist
-    |> Iter.to_list in
+    |> Iter.to_array in
   dist = [| 0; 1; 2; 2 |]
-    && Iter.(for_all (fun i -> List.mem i ord) (0 -- (n - 1)))
-    && Base.List.is_sorted ~compare:(fun v w -> Int.compare dist.(v) dist.(w)) ord
+    && Iter.(for_all (fun i -> Array.mem i ord) (0 -- (n - 1)))
+    && Base.Array.is_sorted ~compare:(fun v w -> Int.compare dist.(v) dist.(w)) ord
 
 let%test "compl_bfs(complete graph)" =
   let n = 4 in
@@ -180,6 +142,6 @@ let%test "compl_bfs(complete graph)" =
   let ord =
     Iter.singleton 0
     |> compl_bfs ~g ~dist
-    |> Iter.to_list in
+    |> Iter.to_array in
   dist = [| 0; -1; -1; -1 |]
-    && ord = [0] (* no ambiguity *)
+    && ord = [| 0 |] (* no ambiguity *)
